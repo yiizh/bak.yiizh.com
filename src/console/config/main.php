8e72d91668583dc018f75efc6ed48ba29ac5a8a9
Di Zhang <zhangdi_me@163.com>
2016-08-27 11:32:19 +0800

    save

---
+++ src/console/config/main.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiizh.com/
+ * @copyright Copyright (c) 2016 yiizh.com
+ * @license http://www.yiizh.com/license/
+ */
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/params.php')
+);
+return [
+    'id' => APP_CONSOLE,
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'console\controllers',
+    'components' => [
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
---
+++ src/console/config/params.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @link http://www.yiizh.com/
+ * @copyright Copyright (c) 2016 yiizh.com
+ * @license http://www.yiizh.com/license/
+ */
+
+return [];
